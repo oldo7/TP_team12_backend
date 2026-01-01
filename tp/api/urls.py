2bc33a4a50c9aba692add6f500fa618ab4fe4dbd
Oliver Nemcek <olivernemcek7@gmail.com>
2026-01-02 00:56:20 +0100

    added more views

--- tp/api/urls.py
+++ tp/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("nodes/", views.getAllNodes.as_view(), name="Get all views"),
+    path("nodes/<int:pk>/", views.getNodeDetail.as_view(), name="node-detail"),
 ]
--- tp/api/views.py
+++ tp/api/views.py
@@ -4,16 +4,26 @@ from rest_framework.response import Response
 from .models import Node
 from .serializers import NodeSerializer
 from rest_framework.views import APIView
+import json
 
-# Create your views here.
+# GET/POST /nodes/
 class getAllNodes(APIView):
     def get(self, request, format=None):
         nodes = Node.objects.all()
         serializer = NodeSerializer(nodes, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+    # example JSON: {"title": "mytitle", "content": "contesttest"}
     def post(self, request):
-        # todo: get data from request
-        n = Node(title="mynewnode", content="contentt")
+        json_body = request.data
+        n = Node(title=json_body["title"], content=json_body["content"])
         n.save()
         return Response("you just posted", status=status.HTTP_200_OK)
+    
+# GET /nodes/<id>/
+class getNodeDetail(APIView):
+    def get(self, request, pk, format=None):
+        nodes = Node.objects.filter(id=pk)
+        print("nodes: ", nodes)
+        serializer = NodeSerializer(nodes, many = True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
