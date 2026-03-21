2858cf4f58c2b22fc4b44c9e2711c71bf7c1d63e
Oliver Nemcek <olivernemcek7@gmail.com>
2026-03-22 00:28:34 +0100

    fixed admin view to correctly view database

--- tp/api/admin.py
+++ tp/api/admin.py
@@ -1,14 +1,53 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import *
-admin.site.register(Node)
-admin.site.register(Technology)
-admin.site.register(Component)
-admin.site.register(DataEntity)
-admin.site.register(Control)
-admin.site.register(ThreatClass)
-admin.site.register(AttackStep)
-admin.site.register(ThreatScenario)
-admin.site.register(DamageScenario)
-admin.site.register(Comporomises)
+
+@admin.register(Component)
+class ComponentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+    filter_horizontal = ['technology', 'communicates_with']
+
+@admin.register(DamageScenario)
+class DamageScenarioAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'component', 'threat_scenario']
+    list_filter = ['component', 'threat_scenario']
+
+@admin.register(Control)
+class ControlAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'component']
+    list_filter = ['component']
+    filter_horizontal = ['attack_steps']
+
+@admin.register(AttackStep)
+class AttackStepAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'component', 'threat_class']
+    list_filter = ['component', 'threat_class']
+    filter_horizontal = ['prepared_by', 'threat_scenario', 'controls']
+
+@admin.register(ThreatScenario)
+class ThreatScenarioAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'threat_class']
+    list_filter = ['threat_class']
+    filter_horizontal = ['attack_step', 'damage_scenario']
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+
+@admin.register(DataEntity)
+class DataEntityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'component']
+    list_filter = ['component']
+    filter_horizontal = ['technology']
+
+@admin.register(ThreatClass)
+class ThreatClassAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+
+@admin.register(Comporomises)
+class ComporomisesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'component', 'threat_scenario', 'compromised_CIA_part']
+    list_filter = ['component', 'threat_scenario']
+
+@admin.register(Node)
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
